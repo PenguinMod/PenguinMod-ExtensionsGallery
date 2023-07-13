38020ea57885904e0c68f41318cde494fb39d9f6
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2023-07-12 22:09:06 -0600

    more basics

---
+++ src/lib/Extension/Component.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let image = "/images/example.png";
+    export let url = "";
+
+    const baseUrl = "https://studio.penguinmod.site/editor.html?extension=";
+</script>
+
+<div>
+    <img src={image} alt="Thumb" />
+    <!-- name -->
+    <p><slot /></p>
+    <button
+        on:click={() => {
+            navigator.clipboard.writeText(url);
+        }}
+    >
+        Copy URL
+    </button>
+    <a href={baseUrl + url} target="_blank">
+        <button>View</button>
+    </a>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
+<script>
+    import Extension from "$lib/Extension/Component.svelte";
+</script>
+
+<img src="/favicon.ico" alt="PenguinMod Logo" />
 <h1>PenguinMod Extra Extensions</h1>
 <p>
     See some cool extensions made by other people here.
@@ -7,3 +12,10 @@
     <a href="/load" target="_blank">load it into PenguinMod,</a>
     or click the "View" button to create a new project with the extension.
 </p>
+
+<Extension
+    image="/images/example.png"
+    url="https://extensions.turbowarp.org/stretch.js"
+>
+    Test
+</Extension>

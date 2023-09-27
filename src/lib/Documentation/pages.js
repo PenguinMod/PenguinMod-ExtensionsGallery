18d2239e799de5a4ca340525dd42431a4bbc114a
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2023-09-26 22:44:36 -0600

    add more detail

--- src/lib/Documentation/pages.js
+++ src/lib/Documentation/pages.js
@@ -1,5 +1,13 @@
+// duplicate the import line and change the variable name
+// then use the path to the md file and then add ?raw
+// ex:
+//      "./test.md?raw"
+//      "./particle-tools.md?raw"
 import PageParticleTools from "./particle-tools.md?raw";
 
 export default {
+    // the key is the path to the docs page
+    // so you can do "sharkpool-particle-tools" for example
+    // you cant use / like "sharkpool/particle-tools" yet
     "particle-tools": PageParticleTools
 };

d28700742cdf5f91ac872d4385bd4190b740f817
Monochromasity <148627069+Monochromasity@users.noreply.github.com>
2023-10-21 13:18:27 +0100

    Create howmanylines.js
    Adds a "How Many Lines in ()" block to the editor. Useful for finding the amount of lines in a user-edited variable.

---
+++ static/extensions/Monochromasity/howmanylines.js
@@ -0,0 +1,38 @@
+// Name: How Many Lines?
+// ID: monochromasityhml
+// Description: Adds a "How Many Lines in ()" block to the editor. Useful for finding the amount of lines in a user-edited variable.
+// By: Monochromasity
+
+(function (Scratch) {
+  "use strict";
+
+  class HML {
+    getInfo() {
+      return {
+        id: 'monochromasityhml',
+        name: 'How Many Lines?',
+        blocks: [
+          {
+            opcode: 'hml',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'how many lines in [INP]',
+            arguments: {
+              INP: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'one line'
+              }
+            }
+          }
+        ]
+      };
+    }
+
+    hml(args) {
+      let inp = args.INP
+      const lines = inp.split('\n');
+      const nonEmptyLines = lines.filter(line => line.trim() !== '');
+      return nonEmptyLines.length;
+    }
+  }
+  Scratch.extensions.register(new HML());
+})(Scratch);

38a96501dde7539aba911fcc8e1004502bd168e2
Monochromasity <148627069+Monochromasity@users.noreply.github.com>
2023-11-08 20:24:30 +0000

    Update howmanylines.js

--- static/extensions/Monochromasity/howmanylines.js
+++ static/extensions/Monochromasity/howmanylines.js
@@ -22,16 +22,32 @@
                 defaultValue: 'one line'
               }
             }
+          },
+          {
+            opcode: 'plusempty',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'how many lines (including empty) in [INP]',
+            arguments: {
+              INP: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: 'one line'
+              }
+            }
           }
         ]
       };
     }
 
     hml(args) {
-      let inp = Scratch.Cast.toString(args.INP)
+      let inp = Scratch.Cast.toString(args.INP);
       const lines = inp.split('\n');
       const nonEmptyLines = lines.filter(line => line.trim() !== '');
       return nonEmptyLines.length;
+    }
+    plusempty(args) {
+      let inp = Scratch.Cast.toString(args.INP);
+      const lines = inp.split('\n');
+      return lines.length;
     }
   }
   Scratch.extensions.register(new HML());

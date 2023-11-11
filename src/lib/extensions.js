f740f5555f848c7263738d3cfe4a632efad08f10
Richard Melenteve <130656326+WAYLIVES@users.noreply.github.com>
2023-11-11 10:24:46 +0300

    Update extensions.js

--- src/lib/extensions.js
+++ src/lib/extensions.js
@@ -7,7 +7,7 @@
 export default [
     {
         name: "FREE Servers",
-        description: "‾‾‾‾‾‾‾‾‾‾‾\nEnglish:\nHere you can find a free server for your projects. And also check whether it is working now or not. \n\nРусский:\nЗдесь вы можете найти бесплатный сервер для своих проектов. А также проверить, работает он сейчас или нет.",
+        description: "‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\nEnglish:\nHere you can find a free server for your projects. And also check whether it is working now or not. \n\nРусский:\nЗдесь вы можете найти бесплатный сервер для своих проектов. А также проверить, работает он сейчас или нет.",
         code: "WAYLIVES/FreeServers.js",
         banner: "WAYLIVES/FreeServers.svg",
         documentation: "FreeServers.md",

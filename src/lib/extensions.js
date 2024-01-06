5d19307de61a9c71703f9ed2469228cfc504b09b
Gen1x-ALT <153012779+Gen1x-ALT@users.noreply.github.com>
2024-01-06 02:21:02 -0300

    add turboweather to extensions.js

--- src/lib/extensions.js
+++ src/lib/extensions.js
@@ -6,6 +6,13 @@
 */
 export default [
     {
+        name: "TurboWeather",
+        description: "Show weather and location data for any place in our world. Data that could be used for doxxing is deleted from results.",
+        code: "RubyDevs/turboweather.js",
+        banner: "RubyDevs/turboweather.png",
+        creator: "Ruby Devs",
+    },
+    {
         name: "Object",
         description: "Handle large JSON files at an extreme speed.",
         code: "skyhigh173/object.js",

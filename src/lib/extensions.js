fa143653c49edb6232f364c4a565acfa67ff5778
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2024-01-18 20:13:07 -0800

    Update extensions.js

--- src/lib/extensions.js
+++ src/lib/extensions.js
@@ -6,6 +6,14 @@
 */
 export default [
     {
+        name: "Pause Utilities",
+        description: "Utilities for (Un)Pausing Scripts, Sprites, and Projects. Can Run Code When Project Paused as Well",
+        code: "SharkPool/Pause-Utilities.js",
+        banner: "SharkPool/Pause-Utilities.svg",
+        creator: "SharkPool-SP",
+        isGitHub: true,
+    },
+    {
         name: "TurboWeather",
         description: "Show weather and location data for any place in our world. Data that could be used for doxxing is deleted from results.",
         code: "RubyDevs/turboweather.js",
@@ -97,14 +105,6 @@ export default [
         description: "Fetch Statistics and Information and Play Songs from Spotify.",
         code: "SharkPool/Spotify.js",
         banner: "SharkPool/Spotify.svg",
-        creator: "SharkPool-SP",
-        isGitHub: true,
-    },
-    {
-        name: "Pause Utilities",
-        description: "Utilities for (Un)Pausing Scripts, Sprites, and Projects. Can Run Code When Project Paused as Well",
-        code: "SharkPool/Pause-Utilities.js",
-        banner: "SharkPool/Pause-Utilities.svg",
         creator: "SharkPool-SP",
         isGitHub: true,
     },

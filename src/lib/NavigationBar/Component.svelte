b83842797fc244838bf2d3df1cbf2b8796e090ec
enderhacker <61245488+enderhacker@users.noreply.github.com>
2024-03-07 18:59:15 +0100

    Update Component.svelte

--- src/lib/NavigationBar/Component.svelte
+++ src/lib/NavigationBar/Component.svelte
@@ -39,7 +39,7 @@
         <img src="/icons/moon.svg" alt="Theme" />
     </BarPage>
     <BarPage link={"/docs"}>Documentation</BarPage>
-    <input type="text" id="searchInput" placeholder="Search..." style="background: var(--penguinmod-color); color: white; border: 1px solid var(--penguinmod-color); border-radius: 5px; padding: 0.5rem; margin-right: 12px;" on:input={searchExtensions} />
+    <input type="text" id="searchInput" placeholder="Search..." style="background: var(--penguinmod-color); color: white; border: 1px solid white; border-radius: 5px; padding: 0.5rem; margin-right: 12px;" on:input={searchExtensions} />
     <BarButton
         highlighted="true"
         link={"https://discord.gg/NZ9MBMYTZh"}

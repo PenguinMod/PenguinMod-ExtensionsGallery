4fe6ec254fbc6dd8ab8df3660248309106a40ac1
mariocraft <154646419+mariocraft987@users.noreply.github.com>
2024-04-04 14:31:55 -0700

    fix spelling error

--- src/lib/extensions.js
+++ src/lib/extensions.js
@@ -2,7 +2,7 @@
     note to contributors & developers that can read JSON:
         the extra commas are added at the end of each thing
         to help copy & pasting work better
-        pleas dont remove them :)
+        please don't remove them :)
 */
 export default [
     {

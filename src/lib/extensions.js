b80081a9c5659fb76a917c295b1551867607b0d0
pooiod7 <82846639+pooiod@users.noreply.github.com>
2024-05-17 23:51:12 -0400

    Update extensions.js

--- src/lib/extensions.js
+++ src/lib/extensions.js
@@ -84,7 +84,7 @@ export default [
     },
     {
         name: "Dictation",
-        description: "Convert your voice into text.",
+        description: "Convert your voice into text. (only works with chrome)",
         code: "pooiod/Dictation.js",
         banner: "pooiod/Dictation.svg",
         creator: "pooiod7",

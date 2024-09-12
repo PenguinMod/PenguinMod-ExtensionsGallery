22df1812be097db5635159972910b2e23d5d31f7
みよ <135030944+yuri-kiss@users.noreply.github.com>
2024-09-12 18:18:32 -0400

    Use jodies original function but merge the 2 files in a compatible way

--- static/extensions/TheShovel/extexp.js
+++ static/extensions/TheShovel/extexp.js
@@ -1,66 +1,86 @@
-class jodieextexp {
+/**!
+ * Extension Exposer
+ * @author TheShovel https://github.com/TheShovel/
+ * @author 0znzw https://scratch.mit.edu/users/0znzw/
+ * @version 1.3
+ * @copyright MIT & LGPLv3 License
+ * @comment Thanks to TheShovel for the original code, I just updated some internals :3
+ * Do not remove this comment
+ */
+(function(Scratch) {
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error(`"Extension Exposer" must be ran unsandboxed.`);
+  }
+  const extId = 'jodieextexp', runText = 'run function [FUNCNAME] from [EXTLIST] with inputs [INPUT]',
+        defaultArguments: {
+          FUNCNAME: { type: ArgumentType.STRING, defaultValue: 'test' },
+          EXTLIST: { type: ArgumentType.STRING, menu: 'EXTLIST', defaultValue: extId },
+          INPUT: { type: ArgumentType.STRING, defaultValue: '{"INPUT":"Hello World!"}' },
+        }, { Cast, BlockType, ArgumentType, vm } = Scratch, { runtime } = vm;
+  class extension {
     getInfo() {
-        let defaultArgs = {
-            FUNCNAME: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue:"test"
-            },
-            EXTLIST: {
-                type: Scratch.ArgumentType.MENU,
-                menu: 'EXTLIST'
-            },
-            INPUT: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue:'"INPUT":"Hello World!"'
-            }
-        };
       return {
-        id: 'jodieextexp',
+        id: extId,
         name: 'Extension Exposer',
-        blocks: [
-          {
-            opcode: 'runcommand',
-            blockType: Scratch.BlockType.COMMAND,
-            text: 'Run function [FUNCNAME] with input [INPUT] in extension [EXTLIST]',
-            arguments: defaultArgs
-          },
-          {
-            opcode: 'runreporter',
-            blockType: Scratch.BlockType.REPORTER,
-            text: 'Run function [FUNCNAME] with input [INPUT] in extension [EXTLIST]',
-            arguments: defaultArgs
-          },
-          {
-            opcode: 'runboolean',
-            blockType: Scratch.BlockType.BOOLEAN,
-            text: 'Run function [FUNCNAME] with input [INPUT] in extension [EXTLIST]',
-            arguments: defaultArgs
-          },
-        ],
+        blocks: [{
+          func: 'run',
+          opcode: 'runcommand',
+          blockType: BlockType.COMMAND,
+          text: runText,
+          arguments: defaultArguments,
+        }, {
+          func: 'run',
+          opcode: 'runreporter',
+          blockType: BlockType.REPORTER,
+          text: runText,
+          arguments: defaultArguments,
+          allowDropAnywhere: true,
+        }, {
+          func: 'run',
+          opcode: 'runboolean',
+          blockType: BlockType.BOOLEAN,
+          text: runText,
+          arguments: defaultArguments,
+          allowDropAnywhere: true,
+        }],
         menus: {
-            EXTLIST: {
-              acceptReporters: true,
-              items: "getExtList"
-            }
-        }
+          EXTLIST: {
+            acceptReporters: true,
+            acceptText: true,
+            items: '_extensions',
+          },
+        },
       };
     }
-    
-    getExtList()  {
-        return Array.from(vm.extensionManager._loadedExtensions.keys()); 
+    _parseJSON(obj) {
+      if (Array.isArray(obj)) return {};
+      if (typeof obj === 'object') return obj;
+      try {
+        obj = JSON.parse(obj);
+        if (Array.isArray(obj)) return {};
+        if (typeof obj === 'object') return obj;
+        return {};
+      } catch {
+        return {};
+      }
     }
-    runcommand(args) {
-        vm.runtime["ext_" + args.EXTLIST][args.FUNCNAME](JSON.parse("{"+args.INPUT+"}"));
-    }
-    runreporter(args) {
-        return vm.runtime["ext_" + args.EXTLIST][args.FUNCNAME](JSON.parse("{"+args.INPUT+"}"));
-    }
-    runboolean(args) {
-        return vm.runtime["ext_" + args.EXTLIST][args.FUNCNAME](JSON.parse("{"+args.INPUT+"}"));
+    _extensions() {
+      const arr = Array.from(vm.extensionManager._loadedExtensions.keys());
+      if (typeof arr[0] !== 'string') arr.push('');
+      return arr;
     }
     test(args) {
-        return args.INPUT
+      return Cast.toString(args.INPUT || ''));
     }
+    run({ FUNCNAME, EXTLIST, INPUT }, util, blockJSON) {
+      XTLIST = Cast.toString(EXTLIST);
+      FUNCNAME = Cast.toString(FUNCNAME);
+      // If the function does not exist then it is not referenced as a real block, or the extension is not global (fallback)
+      return (runtime._primitives[`${EXTLIST}_${FUNCNAME}`] || runtime[`ext_${EXTLIST}`][FUNCNAME])(this._parseJSON(Cast.toString(INPUT)), util, blockJSON);
+    }
+    runcommand() {}
+    runreporter() {}
+    runboolean() {}
   }
-  
-  Scratch.extensions.register(new jodieextexp());
+  Scratch.extensions.register(runtime[`ext_${extId}`] = new extension());
+})(Scratch);

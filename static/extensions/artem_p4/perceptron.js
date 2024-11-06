68f4d0be9c00d7f1021582a89a75e79bccae002b
engineering-wrench <165937828+engineering-wrench@users.noreply.github.com>
2024-11-06 16:16:26 +0300

    Update perceptron.js

--- static/extensions/artem_p4/perceptron.js
+++ static/extensions/artem_p4/perceptron.js
@@ -9,7 +9,7 @@
                            netural-network (scratch)
     credit by: 
     * samllea1 (creator)  yt:https://www.youtube.com/channel/UCPP_LWOC_2mCYxxxJyAi6EQ
-    * artem_p4 (main coder) discord: artem_p4 tg: https://t.me/nine_tailed_fox_01 github: engineering-wrench
+    * artem_p4 (support coder) discord: artem_p4 tg: https://t.me/nine_tailed_fox_01 github: engineering-wrench
     * ChopaChops (support coder) tg: https://t.me/chi4achops
     * open source github - https://github.com/BrainJS/brain.js
     * version: 0.7

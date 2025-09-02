983f7aebf2525854f2da198bc4595b0c1d0532aa
TheShovel <68913917+TheShovel@users.noreply.github.com>
2025-09-02 17:06:55 +0300

    Update extensions.js

--- src/lib/extensions.js
+++ src/lib/extensions.js
@@ -76,7 +76,7 @@ export default [
         name: "Doodle Recognition",
         description: "A implementation of Google's Quick Draw image vision model",
         code: "TheShovel/doodlerec.js",
-        banner: "PuzzlingGGG/doodlerec.png",
+        banner: "TheShovel/doodlerec.png",
         creator: "TheShovel",
         isGitHub: true,
     },

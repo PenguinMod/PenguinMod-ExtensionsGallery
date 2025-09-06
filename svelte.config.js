c346b0c6d56799a421ce0e717576b41c0626b17d
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2025-09-06 15:35:44 -0600

    Revert "fix"
    This reverts commit 6b5b04f6bd03062438f1c3e7ca4b8de30cf71d26.

--- svelte.config.js
+++ svelte.config.js
@@ -6,8 +6,8 @@ export default {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'public',
-			assets: 'public',
+			pages: 'build',
+			assets: 'build',
 			fallback: undefined,
 			precompress: false,
 			strict: true

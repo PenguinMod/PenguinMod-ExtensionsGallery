24b1d5268ae96e76139f646444cd9f8fdcb73088
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2025-09-06 22:33:48 -0600

    format things a bit nicer

--- static/extensions/TheShovel/extexp.js
+++ static/extensions/TheShovel/extexp.js
@@ -1,66 +1,72 @@
-class jodieextexp {
-    getInfo() {
-        let defaultArgs = {
-            FUNCNAME: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue:"test"
-            },
-            EXTLIST: {
-                type: Scratch.ArgumentType.MENU,
-                menu: 'EXTLIST'
-            },
-            INPUT: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue:'{"INPUT":"Hello World!"}'
-            }
-        };
-        return {
-            id: 'jodieextexp',
-            name: 'Extension Exposer',
-            blocks: [
-                {
-                    opcode: 'runcommand',
-                    blockType: Scratch.BlockType.COMMAND,
-                    text: 'run function [FUNCNAME] from [EXTLIST] with inputs [INPUT]',
-                    arguments: defaultArgs
+(function (Scratch) {
+    'use strict';
+    
+    class jodieextexp {
+        getInfo() {
+            const defaultArgs = {
+                FUNCNAME: {
+                    type: Scratch.ArgumentType.STRING,
+                    defaultValue: "test"
                 },
-                {
-                    opcode: 'runreporter',
-                    blockType: Scratch.BlockType.REPORTER,
-                    text: 'run function [FUNCNAME] from [EXTLIST] with inputs [INPUT] ',
-                    arguments: defaultArgs
-                },
-                {
-                    opcode: 'runboolean',
-                    blockType: Scratch.BlockType.BOOLEAN,
-                    text: 'run function [FUNCNAME] from [EXTLIST] with inputs [INPUT]',
-                    arguments: defaultArgs
-                },
-            ],
-            menus: {
                 EXTLIST: {
-                    acceptReporters: true,
-                    items: "getExtList"
+                    type: Scratch.ArgumentType.MENU,
+                    menu: 'EXTLIST'
+                },
+                INPUT: {
+                    type: Scratch.ArgumentType.STRING,
+                    defaultValue:'{"INPUT":"Hello World!"}'
                 }
-            }
-        };
+            };
+
+            return {
+                id: 'jodieextexp',
+                name: 'Extension Exposer',
+                blocks: [
+                    {
+                        opcode: 'runcommand',
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: 'run function [FUNCNAME] from [EXTLIST] with inputs [INPUT]',
+                        arguments: defaultArgs
+                    },
+                    {
+                        opcode: 'runreporter',
+                        blockType: Scratch.BlockType.REPORTER,
+                        text: 'run function [FUNCNAME] from [EXTLIST] with inputs [INPUT]',
+                        arguments: defaultArgs
+                    },
+                    {
+                        opcode: 'runboolean',
+                        blockType: Scratch.BlockType.BOOLEAN,
+                        text: 'run function [FUNCNAME] from [EXTLIST] with inputs [INPUT]',
+                        arguments: defaultArgs
+                    },
+                ],
+                menus: {
+                    EXTLIST: {
+                        acceptReporters: true,
+                        items: "getExtList"
+                    }
+                }
+            };
+        }
+        
+        getExtList() {
+            return Array.from(Scratch.vm.extensionManager._loadedExtensions.keys());
+        }
+        runcommand(args) {
+            Scratch.vm.runtime["ext_" + args.EXTLIST][args.FUNCNAME](JSON.parse(args.INPUT));
+        }
+        runreporter(args) {
+            return Scratch.vm.runtime["ext_" + args.EXTLIST][args.FUNCNAME](JSON.parse(args.INPUT));
+        }
+        runboolean(args) {
+            return Scratch.vm.runtime["ext_" + args.EXTLIST][args.FUNCNAME](JSON.parse(args.INPUT));
+        }
+        
+        test(args) {
+            return args.INPUT;
+        }
     }
     
-    getExtList()  {
-        return Array.from(vm.extensionManager._loadedExtensions.keys()); 
-    }
-    runcommand(args) {
-        vm.runtime["ext_" + args.EXTLIST][args.FUNCNAME](JSON.parse(args.INPUT));
-    }
-    runreporter(args) {
-        return vm.runtime["ext_" + args.EXTLIST][args.FUNCNAME](JSON.parse(args.INPUT));
-    }
-    runboolean(args) {
-        return vm.runtime["ext_" + args.EXTLIST][args.FUNCNAME](JSON.parse(args.INPUT));
-    }
-    test(args) {
-        return args.INPUT
-    }
-}
-
-Scratch.extensions.register(new jodieextexp());
+    Scratch.extensions.register(new jodieextexp());
+})(Scratch);

0300920389d745638212bcc5040807226160e0f8
DogeisCut <23178844+DogeisCut@users.noreply.github.com>
2025-10-03 07:26:47 -0600

    forgot to update this description/comment

--- static/extensions/DogeisCut/dogeiscutObject.js
+++ static/extensions/DogeisCut/dogeiscutObject.js
@@ -1,6 +1,6 @@
 // Name: Objects
 // ID: dogeiscutObject
-// Description: Store data efficiently in multi-purpose objects which support shared and circular references.
+// Description: Store data efficiently in multi-purpose objects.
 // By: dogeiscut <https://scratch.mit.edu/users/dogeiscut/>
 
 // this was a nightmare to code

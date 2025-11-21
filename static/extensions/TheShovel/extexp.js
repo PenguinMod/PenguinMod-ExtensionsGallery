3e69f94e5402298491ae48709cb7cd8122848b64
Miyo Sho <135030944+yuri-kiss@users.noreply.github.com>
2025-11-21 17:50:27 -0500

    fix some of what was mentioned

--- static/extensions/TheShovel/extexp.js
+++ static/extensions/TheShovel/extexp.js
@@ -3,7 +3,7 @@
  * @author TheShovel https://github.com/TheShovel/
  * @author 0znzw https://scratch.mit.edu/users/0znzw/
  * @author Faunksys https://github.com/faunks/
- * @version 2.2
+ * @version 2.3
  * @copyright MIT License
  * Do not remove this comment
  */
@@ -16,12 +16,12 @@
         extId = 'jodieextexp', runText = 'run function [FUNCNAME] from [EXTLIST] with inputs [INPUT]',
         getFunctionsText = 'get blocks from [EXTLIST]',
         defaultArguments = {
-          FUNCNAME: { type: ArgumentType.STRING, defaultValue: 'test' },
-          EXTLIST: { type: ArgumentType.STRING, menu: 'EXTLIST', defaultValue: extId },
-          INPUT: { type: ArgumentType.STRING, defaultValue: '{"INPUT":"Hello World!"}' },
+          FUNCNAME: { type: ArgumentType.STRING, defaultValue: 'movesteps' },
+          EXTLIST: { type: ArgumentType.STRING, menu: 'EXTLIST' },
+          INPUT: { type: ArgumentType.STRING, defaultValue: '{"STEPS":10}' },
         },
         getBlocksArgument = {
-          EXTLIST: { type: ArgumentType.STRING, menu: 'EXTLIST', defaultValue: extId },
+          EXTLIST: { type: ArgumentType.STRING, menu: 'EXTLIST' },
         };
   
   const isPackaged = ('scaffolding' in globalThis) && !Scratch.gui;
@@ -76,7 +76,8 @@
         },
       };
     }
-    _parseJSON(obj) {
+    // NOTE: This is only meant to return Object values and nothing else (no Arrays or null)
+    _convertToObject(obj) {
       if (typeof obj === 'object') {
         if (
           obj === null ||
@@ -95,26 +96,29 @@
         return {};
       }
     }
+    static _BUILT_IN_CATEGORIES = [
+      // Built in categories.
+      // The "scratch3_" prefix can be added if you want to access direct functions on the classes.
+      `motion`,
+      `looks`,
+      `sound`,
+      `event`,
+      `control`,
+      `sensing`,
+      `operators`,
+      `data`,
+      `procedures`,
+    ];
     _extensions() {
       const arr = Array.from(extensionManager._loadedExtensions.keys()).map(id => String(id));
-      return [
-        // Built in categories.
-        `scratch3_motion`,
-        `scratch3_looks`,
-        `scratch3_sound`,
-        `scratch3_event`,
-        `scratch3_control`,
-        `scratch3_sensing`,
-        `scratch3_operators`,
-        `scratch3_data`,
-        `scratch3_procedures`,
-      ].concat(arr);
+      return jodieextexp._BUILT_IN_CATEGORIES.concat(arr);
     }
     _getExtensionObject(id) {
       // TurboWarp and PenguinMod export style.
       let ext = runtime[`ext_${id}`];
       if (ext) return ext;
       // Unsandboxed (mod) export style.
+      // NOTE: This is only added because a large amount of extensions use it.
       ext = runtime[`cext_${id}`];
       if (ext) return ext;
       return null;
@@ -123,27 +127,21 @@
       EXTLIST = Cast.toString(EXTLIST);
       FUNCNAME = Cast.toString(FUNCNAME);
 
-      // Blocks have priority over built-in functions.
+      // Real blocks have priority over class functions.
       let fn = runtime._primitives[`${EXTLIST}_${FUNCNAME}`];
       if (!fn) {
+        // "scratch3_" will fall through to here, so no if check is needed.
         const ext = this._getExtensionObject(EXTLIST);
         fn = (typeof ext[FUNCNAME] === 'function') && ext[FUNCNAME].bind(ext);
       }
       
       // If the function does not exist then the function does not exist on the target extension, or the extension lied about existing.
-      return fn(this._parseJSON(Cast.toString(INPUT)), util, blockJSON);
+      return fn(this._convertToObject(Cast.toString(INPUT)), util, blockJSON);
     }
     getBlocks({ EXTLIST }) {
-      const ext = this._getExtensionObject(Cast.toString(EXTLIST));
-      if (!ext) return [];
-      if (typeof ext.getInfo === 'function') {
-        return (ext.getInfo().blocks || []).flatMap(block => (
-          block && (typeof block.opcode === 'string') ? [block.opcode] : []
-        ));
-      } else if (typeof ext.getPrimitives === 'function') {
-        return Object.getOwnPropertyNames(ext.getPrimitives()).map(opcode => String(opcode));
-      }
-      return [];
+      EXTLIST = `${Cast.toString(EXTLIST)}_`;
+      // We use the primitives list here to make sure we aren't getting say.. buttons or labels, which are not really "blocks" in the traditional sense.
+      return Object.getOwnPropertyNames(runtime._primitives).filter(opcode => opcode.startsWith(EXTLIST));
     }
 
     runcommand() { return ''; }
@@ -152,6 +150,7 @@
 
     getfunctions() { return ''; }
 
+    // @depricated
     test({ INPUT }) {
       return Cast.toString(INPUT);
     }
@@ -169,21 +168,24 @@
       } else {
         this._strict[0] = true;
         this._strict[1] = this.run;
-        this.run = function run({ FUNCNAME, EXTLIST }) {
-          EXTLIST = Cast.toString(EXTLIST);
-          FUNCNAME = Cast.toString(FUNCNAME);
+        this.run = function run(args) {
+          args.EXTLIST = Cast.toString(args.EXTLIST);
+          args.FUNCNAME = Cast.toString(args.FUNCNAME);
 
-          if (!runtime._primitives[`${EXTLIST}_${FUNCNAME}`]) {
-            console.error('The block a block tried to use does not exist.', EXTLIST, FUNCNAME);
-            if (runtime.ext_pm_liveTests) throw new ReferenceError('The block a block tried to use does not exist.');
+          // Force old "scratch3_" style inputs into valid primitive categories.
+          if (jodieextexp._BUILT_IN_CATEGORIES.includes(args.EXTLIST)) args.EXTLIST = args.EXTLIST.replace('scratch3_', '');
+          
+          if (!runtime._primitives[`${args.EXTLIST}_${args.FUNCNAME}`]) {
+            console.error('The block a block tried to use does not exist.', args.EXTLIST, args.FUNCNAME);
+            if (Scratch.extensions.isPenguinMod) throw new ReferenceError('The block a block tried to use does not exist.');
             return '';
           }
 
-          if (runtime.ext_pm_liveTests) return this._strict[1].apply(this, arguments);
+          if (Scratch.extensions.isPenguinMod) return this._strict[1].apply(this, arguments);
           try {
             return this._strict[1].apply(this, arguments) ?? '';
           } catch(error) {
-            console.error('The block or extension a block tried to use failed to run.', EXTLIST, FUNCNAME, error);
+            console.error('The block or extension a block tried to use failed to run.', args.EXTLIST, args.FUNCNAME, error);
             return '';
           }
         };
@@ -193,7 +195,5 @@
     }
   }
 
-  const instance = (runtime[`ext_${extId}`] = new jodieextexp());
-  runtime._primitives[`${extId}_test`] = instance.test.bind(instance);
-  Scratch.extensions.register(instance);
+  Scratch.extensions.register(runtime[`ext_${extId}`] = new jodieextexp());
 })(Scratch);

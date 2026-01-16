b82cced6ec0e8d5731947acb8f9d5cde89cd4e02
2015kaito8169 <student1259@school.tahara.ed.jp>
2026-01-16 15:07:51 +0900

    Update extensions.js

--- src/lib/extensions.js
+++ src/lib/extensions.js
@@ -571,11 +571,11 @@ export default [
         banner: "Gen1x/mw-placeholder.avif",
         creator: "G1nX",
     },
-    {
+        {
         name: "AES",
         description: "Encrypt and decrypt strings with a custom password. Supports Japanese and English characters.",
         code: "2015kaito8169/AES.js",
-        banner: "favicon.avif",
+        banner: "aes-banner.avif",
         creator: "2015kaito8169"
     }
 ];

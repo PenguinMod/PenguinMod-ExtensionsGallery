3ad93a097131d7a52c9c55cf452e001a7d38c869
FloppyDisk_OSC <saavedrajr96@gmail.com>
2026-01-23 16:27:43 -0500

    Update extensions.js

--- src/lib/extensions.js
+++ src/lib/extensions.js
@@ -581,10 +581,11 @@ export default [
     },
     */
     {
-     name: "Smoke Detector",
-    description: "Plays a beep every 30 seconds.",
-    code: "electricfuzzball_pm/firealarm.js",
-    banner: "electricfuzzball_pm/firealarm.svg",
-    creator: "ElectricFuzzball_PM", //hey look thats me!!
+        name: "Smoke Detector",
+        description: "Plays a beep every 30 seconds.",
+        code: "electricfuzzball_pm/firealarm.js",
+        banner: "electricfuzzball_pm/firealarm.svg",
+        creator: "ElectricFuzzball_PM" //hey look thats me!!
+        //Pluey has been implemented.
     }
 ];

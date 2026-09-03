bcb7b26e831a77f8f02b864573421a28061f26a6
Nishi <nishi@nishi.boats>
2026-09-04 00:30:29 +0900

    format

--- static/extensions/NishiOwO/ode.js
+++ static/extensions/NishiOwO/ode.js
@@ -167,30 +167,33 @@
     Scratch.BlockType[Scratch.extensions.isNitroBolt ? "ARRAY" : "REPORTER"];
   const arg_array =
     Scratch.ArgumentType[Scratch.extensions.isNitroBolt ? "ARRAY" : "STRING"];
-  const from_array = (Scratch.extensions.isNitroBolt || Scratch.extensions.isPenguinMod)
-    ? (x) => x
-    : (x) => JSON.stringify(x);
+  const from_array =
+    Scratch.extensions.isNitroBolt || Scratch.extensions.isPenguinMod
+      ? (x) => x
+      : (x) => JSON.stringify(x);
   const to_f32array = Scratch.extensions.isNitroBolt
     ? Scratch.Cast.toFloat32Array
-    : (Scratch.extensions.isPenguinMod && jwArray)
-    ? (x) => {
-	return new jwArray.Type(x);
-    } : (x) => {
-        try {
-          const json = JSON.parse(x);
-
-          if (!Array.isArray(json)) return new Float32Array([]);
-
-          return json;
-        } catch {
-          return new Float32Array([]);
+    : Scratch.extensions.isPenguinMod && jwArray
+      ? (x) => {
+          return new jwArray.Type(x);
         }
-      };
+      : (x) => {
+          try {
+            const json = JSON.parse(x);
+
+            if (!Array.isArray(json)) return new Float32Array([]);
+
+            return json;
+          } catch {
+            return new Float32Array([]);
+          }
+        };
 
   if (Scratch.vm.divVecQuat) divVecQuat = Scratch.vm.divVecQuat;
 
-  if (Scratch.extensions.isPenguinMod && !Scratch.vm.jwArray) Scratch.vm.extensionManager.loadExtensionIdSync("jwArray");
-  if (Scratch.vm.jwArray){
+  if (Scratch.extensions.isPenguinMod && !Scratch.vm.jwArray)
+    Scratch.vm.extensionManager.loadExtensionIdSync("jwArray");
+  if (Scratch.vm.jwArray) {
     jwArray = Scratch.vm.jwArray;
     blk_array_obj = jwArray.Block;
     arg_array_obj = jwArray.Argument;
@@ -3442,7 +3445,7 @@ embedded = true;
               GRAVITY: {
                 type: arg_array,
                 defaultValue: from_array([0, -9.81, 0]),
-		...arg_array_obj
+                ...arg_array_obj,
               },
             },
           },
@@ -3460,12 +3463,12 @@ embedded = true;
               START: {
                 type: arg_array,
                 defaultValue: from_array([0, 0, 0]),
-		...arg_array_obj
+                ...arg_array_obj,
               },
               END: {
                 type: arg_array,
                 defaultValue: from_array([0, 0, 0]),
-		...arg_array_obj
+                ...arg_array_obj,
               },
             },
           },
@@ -3483,12 +3486,12 @@ embedded = true;
               START: {
                 type: arg_array,
                 defaultValue: from_array([0, 0, 0]),
-		...arg_array_obj
+                ...arg_array_obj,
               },
               END: {
                 type: arg_array,
                 defaultValue: from_array([0, 0, 0]),
-		...arg_array_obj
+                ...arg_array_obj,
               },
               GEOM: {
                 type: Scratch.ArgumentType.STRING,
@@ -3528,7 +3531,7 @@ embedded = true;
           {
             opcode: "bodyGetArray",
             blockType: blk_array,
-	    ...blk_array_obj,
+            ...blk_array_obj,
             disableMonitor: true,
             text: Scratch.translate("get [TYPE] of body [BODY]"),
             arguments: {
@@ -3560,7 +3563,7 @@ embedded = true;
               ARRAY: {
                 type: arg_array,
                 defaultValue: from_array([0, 0, 0]),
-		...arg_array_obj
+                ...arg_array_obj,
               },
             },
           },
@@ -3576,7 +3579,7 @@ embedded = true;
               FORCE: {
                 type: arg_array,
                 defaultValue: from_array([0, 0, 0]),
-		...arg_array_obj
+                ...arg_array_obj,
               },
             },
           },
@@ -3666,7 +3669,7 @@ embedded = true;
               SIZE: {
                 type: arg_array,
                 defaultValue: from_array([1, 1, 1]),
-		...arg_array_obj
+                ...arg_array_obj,
               },
               WORLD: {
                 type: Scratch.ArgumentType.STRING,
@@ -3777,12 +3780,12 @@ embedded = true;
               VERTEX: {
                 type: arg_array,
                 defaultValue: from_array([]),
-		...arg_array_obj
+                ...arg_array_obj,
               },
               INDEX: {
                 type: arg_array,
                 defaultValue: from_array([]),
-		...arg_array_obj
+                ...arg_array_obj,
               },
               WORLD: {
                 type: Scratch.ArgumentType.STRING,
@@ -3811,7 +3814,7 @@ embedded = true;
               DATA: {
                 type: arg_array,
                 defaultValue: from_array([]),
-		...arg_array_obj
+                ...arg_array_obj,
               },
               WIDTH: {
                 type: Scratch.ArgumentType.NUMBER,
@@ -3882,7 +3885,7 @@ embedded = true;
           {
             opcode: "geomGetArray",
             blockType: blk_array,
-	    ...blk_array_obj,
+            ...blk_array_obj,
             disableMonitor: true,
             text: Scratch.translate("get [TYPE] of geometry [GEOM]"),
             arguments: {
@@ -3914,7 +3917,7 @@ embedded = true;
               ARRAY: {
                 type: arg_array,
                 defaultValue: from_array([0, 0, 0]),
-		...arg_array_obj
+                ...arg_array_obj,
               },
             },
           },
@@ -3959,7 +3962,7 @@ embedded = true;
           {
             opcode: "jointGetArray",
             blockType: blk_array,
-	    ...blk_array_obj,
+            ...blk_array_obj,
             disableMonitor: true,
             text: Scratch.translate("get [TYPE] of joint [JOINT]"),
             arguments: {
@@ -4073,7 +4076,7 @@ embedded = true;
               TORQUES: {
                 type: arg_array,
                 defaultValue: from_array([0]),
-		...arg_array_obj
+                ...arg_array_obj,
               },
             },
           },
